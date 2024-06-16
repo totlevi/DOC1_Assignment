5c9bfbdba436e8c335dbc2c50d7387708dd3c493
Ágoston Babicz <babiczagoston3@gmail.com>
2024-06-17 00:04:08 +0200

    CI Pipeline - Rewrite tests(#5)

--- React/src/tests/App.test.js
+++ React/src/tests/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../components/App';
 
 test('renders the header', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
   const headerElement = screen.getByText(/Welcome to VIA tabloid app/i);
   expect(headerElement).toBeInTheDocument();
 });
