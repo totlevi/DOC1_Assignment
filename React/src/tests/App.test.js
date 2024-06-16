5636f0f28519c3188915a865a352035b1292a88d
Ágoston Babicz <babiczagoston3@gmail.com>
2024-06-16 23:42:37 +0200

    CI Pipeline - Rewrite tests(#3)

--- React/src/tests/App.test.js
+++ React/src/tests/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from '../components/App';
 
-test('renders', () => {
+test('renders the header', () => {
   render(<App />);
+  const headerElement = screen.getByText(/Welcome to VIA tabloid app/i);
+  expect(headerElement).toBeInTheDocument();
 });
